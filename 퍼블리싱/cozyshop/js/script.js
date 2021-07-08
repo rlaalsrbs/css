b29bab33a2b68b81d845269f8eb9d5489e8355c1
rlaalsrbs <iceblue086@gmail.com>
2021-07-08 18:12:13 +0900

    CSS 학습

--- 퍼블리싱/cozyshop/js/script.js
+++ 퍼블리싱/cozyshop/js/script.js
@@ -1,8 +1,26 @@
 window.onload = function() {
 var swiper = new Swiper(".main_banner .mySwiper", {
         navigation: {
-          nextEl: ".main_banner .swiper-button-next",
-          prevEl: ".main_banner .swiper-button-prev",
+          nextEl: ".main_banner .next",
+          prevEl: ".main_banner .prev",
+        },
+		pagination: {
+			el: ".main_banner .swiper-pagination",
+			clickable:true,
+		}
+      });
+	  
+	   var swiper2 = new Swiper(".tv_movie .mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 0,
+		 navigation: {
+          nextEl: ".tv_movie .next",
+          prevEl: ".tv_movie .prev",
+        },
+        pagination: {
+          el: ".tv_movie .swiper-pagination",
+          clickable: true,
         },
       });
+	  
 };	  
