b3191412de99e63bcb9576b73d927c4ada97da28
rlaalsrbs <iceblue086@gmail.com>
2021-07-12 07:54:56 +0900

    html css 시험

--- 퍼블리싱/cozyshop/js/script.js
+++ 퍼블리싱/cozyshop/js/script.js
@@ -1,26 +1,26 @@
 window.onload = function() {
-var swiper = new Swiper(".main_banner .mySwiper", {
-        navigation: {
-          nextEl: ".main_banner .next",
-          prevEl: ".main_banner .prev",
-        },
+	var swiper = new Swiper(".main_banner .mySwiper", {
+		navigation: {
+			nextEl: ".main_banner .next",
+			prevEl: ".main_banner .prev",
+		},
 		pagination: {
-			el: ".main_banner .swiper-pagination",
-			clickable:true,
-		}
-      });
-	  
-	   var swiper2 = new Swiper(".tv_movie .mySwiper", {
+          el: ".main_banner .swiper-pagination",
+		  clickable : true,
+        },
+		loop : true,
+	});
+	
+	 var swiper2 = new Swiper(".tv_movie .mySwiper", {
         slidesPerView: 5,
         spaceBetween: 0,
-		 navigation: {
-          nextEl: ".tv_movie .next",
-          prevEl: ".tv_movie .prev",
-        },
+		navigation: {
+			nextEl: ".tv_movie .next",
+			prevEl: ".tv_movie .prev",
+		},
         pagination: {
           el: ".tv_movie .swiper-pagination",
           clickable: true,
         },
       });
-	  
-};	  
+};
